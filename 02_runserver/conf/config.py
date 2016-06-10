cf09b3637505758744ce11f484c0ed0926c85c75
Vera Mazhuga <vmazhuga@tappsi.co>
2016-06-10 10:02:44 -0500

    02 runserver

---
+++ 02_runserver/conf/config.py
@@ -0,0 +1,12 @@
+import sys
+
+DEBUG = False
+
+try:
+    if 'test' in sys.argv:
+        from test_config import *
+    else:
+        from local_config import *
+
+except ImportError, e:
+    pass
---
+++ 02_runserver/manage.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask.ext.script import Manager
+
+from run import create_app
+
+app = create_app()
+manager = Manager(app)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    manager.run()
