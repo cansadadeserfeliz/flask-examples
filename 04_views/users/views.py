4f3c173b7150ff3cc20e9444d8941253f7df81be
Vera Mazhuga <vero4ka.ru@gmail.com>
2016-07-04 20:29:54 -0500

    O4 views

--- 04_views/users/views.py
+++ 04_views/users/views.py
@@ -1,24 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template
 
 
 users = Blueprint('users', __name__, template_folder='templates')
 
 
-@users.route("/")
-def hello():
-    return "Hello Worlds!"
-
-
-@users.route('/user/<name>')
-def user_profile(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
-
-
 @users.route('/user/<int:pk>')
 def user_detail(pk):
-    return '<h1>Hello, user #{}!</h1>'.format(pk)
-
-
-@users.route('/user/<int:pk>/create', methods=('POST',))
-def user_create(pk):
-    pass
+    print request
+    print request.method  # ['GET', 'POST', ...]
+    print request.args
+    print request.headers
+    return render_template(
+        'users/detail.html',
+        user_pk=pk,
+    )
